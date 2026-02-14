d50586eceba7a143e9ce9346594c72363e048480
Ariel Soriano Vassia <lrarielsoriano@gmail.com>
2026-02-14 15:34:07 +0100

    feat(domain): add FilterEventsByCategoryUseCase
    Create use case to filter events by category through repository interface

---
+++ lib/src/features/events/domain/usecases/filter_events_by_category.dart
@@ -0,0 +1,13 @@
+import 'package:city_events_explorer/src/features/events/domain/entities/event.dart';
+import 'package:city_events_explorer/src/features/events/domain/repositories/i_event_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class FilterEventsByCategoryUseCase {
+  FilterEventsByCategoryUseCase(this._repository);
+
+  final IEventRepository _repository;
+
+  Future<List<Event>> call(String category) =>
+      _repository.filterByCategory(category);
+}
